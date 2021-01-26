fbe2fb794bfbb64f1a7da75d9d058ee99982da97
Saptarshi Majumdar <saptarshi3.14@gmail.com>
2021-01-26 09:43:33 +0000

    Status: Working Adding feature to create sheet name based on date and picking out this sheet

--- ICRallocation-automate.R
+++ ICRallocation-automate.R
@@ -3,33 +3,42 @@ library(data.table)
 library(stringr)
 
 # Read Data collection/latest allocation sheet ----------------
-oxcgrt_datacollection_url <- "https://docs.google.com/spreadsheets/d/1D2ZJcmX0LQVzW9kiyyRrIN8SeuqMlcfcaX9eyK2vqfI/edit#gid=2022852213"
+#oxcgrt_datacollection_url <- "https://docs.google.com/spreadsheets/d/1D2ZJcmX0LQVzW9kiyyRrIN8SeuqMlcfcaX9eyK2vqfI/edit#gid=2022852213"
 
-oxcgrt_datacollection <- googlesheets4::range_read("1D2ZJcmX0LQVzW9kiyyRrIN8SeuqMlcfcaX9eyK2vqfI", sheet= "19 January 2021")
+# Process Date to pick out latest sheet -------------------------
+current_date <- Sys.Date()
+dayofweek <- as.POSIXlt(current_date)$wday
+alloc_date <- current_date - (dayofweek - 2)
+m <- month.name[month(alloc_date)]
+y <- year(alloc_date)
+d <- yday(alloc_date)
+sheet_name <- paste(yday(alloc_date), m, y)
+
+oxcgrt_datacollection <- googlesheets4::range_read("1D2ZJcmX0LQVzW9kiyyRrIN8SeuqMlcfcaX9eyK2vqfI", sheet= sheet_name)
 oxcgrt_datacollection <- as.data.table(oxcgrt_datacollection)
 
 # List all the countries that are currently being allocated -----------
 oxcgrtdata <- unique(fread("https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv",
                            select = c("CountryCode")))
 
-# remove any NAs from update column
+# remove any NAs from update column--------------------
 oxcgrt_datacollection <- oxcgrt_datacollection[!is.na(Update)]
-
-# Get random sample of 10 contributors
+i
+# Get random sample of 10 contributors --------------------------
 ICR_allocation <- data.table(Name = c(sample(oxcgrt_datacollection$Name, 15, replace = F)))
 
-# Which countries are they currently allocated to update? 
+# Which countries are they currently allocated to update? --------------------------------
 ICR_allocation <- oxcgrt_datacollection[ICR_allocation, on = .(Name), .(Name, Update)]
 
-# Cleaning up Update column in case of collateral strings like "Build from.."
+# Cleaning up Update column in case of collateral strings like "Build from.." -------------------------
 ICR_allocation <- ICR_allocation[, Update := str_remove_all(Update, " ")
                                  ][, Update := ifelse(str_length(Update) > 15, str_extract(Update, "[A-Z]{3}"), Update)
                                    ][, Update := str_replace(Update, ",", "|")]
 
-# Generating random allocations and Drop Update Allocation
+# Generating random allocations and Drop Update Allocation -----------------------------
 ICR_allocation <- ICR_allocation[, Allocation := lapply(ICR_allocation$Update, function(x) sample(str_subset(oxcgrtdata$CountryCode, x, negate = T), 1))
                                  ][,.(Name, Allocation)] 
 
-# output to a csv 
+# output to a csv ------------------------
 fwrite(ICR_allocation, "./InterCoder_Allocation.csv")
 
