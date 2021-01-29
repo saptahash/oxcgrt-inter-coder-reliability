0e5917e4ad56433aedcd84838c8ee4cbc74564c3
Saptarshi Majumdar <saptarshi3.14@gmail.com>
2021-01-29 12:12:55 +0000

    Adding the latest ICR allocation sheet for 29-01. No changes made to the code pending feats - pull from pods sheet to get each person's pods and then randomise within the pods for 0.5 of the contributors

--- ICRallocation-automate.R
+++ ICRallocation-automate.R
@@ -17,13 +17,19 @@ sheet_name <- paste(yday(alloc_date), m, y)
 oxcgrt_datacollection <- googlesheets4::range_read("1D2ZJcmX0LQVzW9kiyyRrIN8SeuqMlcfcaX9eyK2vqfI", sheet= sheet_name)
 oxcgrt_datacollection <- as.data.table(oxcgrt_datacollection)
 
+# Get Pod IDs for the contributors --------------------------------
+contributor_pods <- googlesheets4::range_read("1D2ZJcmX0LQVzW9kiyyRrIN8SeuqMlcfcaX9eyK2vqfI", sheet= "Who does what")
+contributor_pods <- as.data.table(contributor_pods)[!is.na(Name),.(Name, Group)]
+                                                    
+contributor_pods[, PodID := lapply(contributor_pods$Group, function(x) ifelse(!is.na(x), str_subset(x, "[0-9]*$"), NA))]
+
 # List all the countries that are currently being allocated -----------
 oxcgrtdata <- unique(fread("https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv",
                            select = c("CountryCode")))
 
 # remove any NAs from update column--------------------
 oxcgrt_datacollection <- oxcgrt_datacollection[!is.na(Update)]
-i
+
 # Get random sample of 10 contributors --------------------------
 ICR_allocation <- data.table(Name = c(sample(oxcgrt_datacollection$Name, 15, replace = F)))
 
